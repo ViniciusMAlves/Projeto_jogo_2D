9e15d831c8c89cbb6dc8c0e8ef6169f855602f1b
Vinicius Moreira Alves <37082519+ViniciusMAlves@users.noreply.github.com>
2023-06-15 22:53:56 -0300

    boss
    modificado a barra de vida do boss

--- Projeto1/Scripts/Boss.gd
+++ Projeto1/Scripts/Boss.gd
@@ -3,10 +3,19 @@ extends enemyBase
 signal BossDead
 
 var life_size = 7
+var lifeBar_size = 2
 
 func _ready():
 	set_physics_process(false)
 	delay = 0.7
+	$bar.set_size(Vector2((lifeBar_size * health)+2,(lifeBar_size+2)))
+	$bar.position.y = $bar.position.y - 1
+	$bar.position.x = -1*(((lifeBar_size * health)+2)/2)
+	
+	$lifebar.set_size(Vector2(lifeBar_size * health,(lifeBar_size)))
+	$lifebar.position.x = -1*((lifeBar_size * health)/2)
+	
+	
 	$lifes.set_size(Vector2(life_size * health,0))
 	$lifes.position.x = -1*((life_size * health)/2)
 
@@ -32,6 +41,7 @@ func _set_boss_animation():
 	
 	if  hitted:
 		anim = "hit"
+		$lifebar.set_size(Vector2(lifeBar_size * health,(lifeBar_size)))
 		$lifes.set_size(Vector2(life_size * health,0))
 		$lifes.position.x = -1*((life_size * health)/2)
 		
