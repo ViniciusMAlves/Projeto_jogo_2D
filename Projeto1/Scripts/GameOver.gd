afa7b12f592526baefd7a0737853e3bed84513a0
Vinicius Moreira Alves <37082519+ViniciusMAlves@users.noreply.github.com>
2024-05-22 21:21:16 -0300

    atualização
    a Godot atualizou

--- Projeto1/Scripts/GameOver.gd
+++ Projeto1/Scripts/GameOver.gd
@@ -1,5 +1,13 @@
 extends CanvasLayer
 
+func _ready():
+	$Control/btnReturn.grab_focus()
+
 func _on_btn_return_pressed():
 	Global.checkpoint_pos = Vector2(0, 0)
+	Global.player_life = 3
 	get_tree().change_scene_to_file(Global.sena_atual)
+
+
+func _on_btn_menu_pressed():
+	get_tree().change_scene_to_file("res://Levels/start_screen.tscn")
